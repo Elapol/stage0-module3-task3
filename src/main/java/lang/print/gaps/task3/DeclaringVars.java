e4d3acac947cdef604e03fba2acea1bc23f61f48
elyad <ella.dikovich@gmail.com>
2022-10-25 16:41:55 +0200

    !!

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -2,7 +2,12 @@ package lang.print.gaps.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int a, b, c = 10;
+        int a;
+        a = 10;
+        int b;
+        b = 10;
+        int c;
+        c = 10;
         System.out.println(c);
     }
 }
