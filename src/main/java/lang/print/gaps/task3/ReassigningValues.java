449fcf0c3fe3288474fb1bdff0e71483ae4a29f4
elyad <ella.dikovich@gmail.com>
2022-10-25 16:35:17 +0200

    yes

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,6 +2,21 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first;
+        int second;
+        int third;
+        first = 1;
+        second = 10;
+        third = 100;
+        System.out.println("first\n" + "second\n" + "third");
+        int linkToFirst = 15;
+        first = linkToFirst;
+        int linkToSecond = 6;
+        second = linkToSecond;
+        int linkToThird = 4;
+        third = linkToThird;
+        System.out.println(first + "\n" + second + "\n" + third);
+        
+
     }
 }
